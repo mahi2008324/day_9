450776e4e50d8fa961edc1deb6af183b1163e1e4
mahi2008324 <seshadrinath54054@gmail.com>
2025-04-30 12:59:02 +0530

    assign_operative

---
+++ assign_operative_1.py
@@ -0,0 +1,10 @@
+#assignment "="
+a=8.1
+print(a)
+print(type(a))
+print(id(a))
+b=81
+print(b)
+print(type(b))
+print(id(b))
+	
---
+++ assign_operative_2.py
@@ -0,0 +1,10 @@
+#assignment "+="
+a=8.1
+print(a)
+print(type(a))
+print(id(a))
+a+=81
+print(a)
+print(type(a))
+print(id(a))
+	
